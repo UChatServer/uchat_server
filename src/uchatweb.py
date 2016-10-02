100181faee4a17b71a794a49ee2b188a066afd1f
kongyt <839339849@qq.com>
2016-10-02 12:10:47 -0400

    解压了web.py-0.38.tar.gz

--- src/uchatweb.py
+++ src/uchatweb.py
@@ -5,6 +5,17 @@ from uchatserver import *
 from uchatdb import *
 import json
 
+##返回信息
+##{"code": num,
+## "result": value
+## "err_str": str
+##}
+## code信息
+##1表示执行操作成功,result中为返回的数据
+##0表示执行操作失败，err_str中会有错误提示
+
+
+
 
 app_key = "x18ywvqf8x8rc"
 app_secret = "I5Rty3m89YC"
@@ -24,11 +35,14 @@ class login:
         global ucs
         global ucdb
         i = web.input()
-        userid = i.user_id
-        password = i.user_password
+        userid = i.id
+        password = i.password
+
+        web.header('content-type','text/json')
         rs = ucdb.canlogin(userid, password)
         if rs[0] is False:
-            return ("不能登录：%s" % rs[1]).decode("utf8").encode('gb2312')
+            error_str = "不能登录：%s" % rs[1]
+            return json.dumps({"code":0, "result":"null", "err_str": error_str})
         else:
             rs = ucdb.isonline(userid)
             if rs[0] is False:
@@ -38,18 +52,21 @@ class login:
                 print rs
                 if rs.result[u'code'] is 200:
                     ucdb.online(userid, rs.result[u'token'])
-                    return "成功".decode("utf8").encode('gb2312')+str(rs)
+                    return json.dumps({"code":1, "result": rs.result[u'token'], "err_str": "null"})
                 else:
-                    return "登录失败：融云服务返回Token失败".decode("utf8").encode('gb2312')+str(rs)
+                    error_str = "登录失败：融云服务返回Token失败"
+                    return json.dumps({"code":0, "result": "null", "err_str": error_str})
             else:
-                return "登录失败：用户已经在线".decode('utf8').encode('gb2312')
+                error_str =  "登录失败：用户已经在线"
+                return json.dumps({"code":0, "result": "null", "err_str": error_str})
 class register:
     def GET(self):
         global ucs
         global ucdb
         i = web.input()
-        userid = i.user_id
-        password = i.user_password
+        userid = i.id
+        password = i.password
+        web.header('content-type','text/json')
         rs = ucdb.register(userid, password)
         if rs[0] is False:
             return ("注册失败：%s" % rs[1]).decode('utf8').encode('gb2312')
@@ -62,8 +79,9 @@ class logout:
         global ucs
         global ucdb
         i = web.input()
-        userid = i.user_id
-        usertoken = i.user_token
+        userid = i.id
+        usertoken = i.token
+        web.header('content-type','text/json')
         rs = ucdb.isonline(userid)
         if rs[0] is False:
             return ('退出失败：%s' % rs[1]).decode('utf8').encode('gb2312')
@@ -76,6 +94,12 @@ class logout:
 
 class reconnect:
     def GET(self):
+        global ucs
+        global ucdb
+        i = web.input()
+        userid = i.id
+        usertoken = i.token
+        web.header('content-type','text/json')
         return "敬请期待".decode('utf8').encode('gb2312')
 
 
